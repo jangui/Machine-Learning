5105d223f09f103788c39490d3705ec55d137ce9
jangui <jaimitino@gmail.com>
2020-01-26 16:03:01 -0500

    file structuring

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Machine-Learning
-A repo with different ML algorithms
+# Nerva
+A machine learning library
--- nerva/optimizers.py
+++ nerva/optimizers.py
@@ -27,3 +27,22 @@ class GradientDescent:
         plt.ylabel("Cost")
         plt.title("Cost over Iterations")
         plt.show()
+
+class NormalEquation:
+        """
+        Closed form solution for linear Regression
+        No need to normalize before using
+        Note: slow when number of features is high (above 10,000ish)
+        """
+    def __init__(self, X, y, theta, cost, alpha, itr, plot=False):
+        self.X = X
+        self.y = y
+        self.theta = theta
+
+    def optimize(self):
+        inv = np.linalg.inv(np.dot(self.X.T, self.X))
+        self.theta = np.dot(np.dot(inv, self.X.T), self.y)
+        return self.theta
+
+class SGD:
+    pass
